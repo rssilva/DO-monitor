7d68e8cd8f9d33a20d1971004e460ccf8ee976b7
rssilva <rafaelspdasilva@gmail.com>
2015-05-31 23:10:42 -0300

    adding digitalOcean wrapper instantiation to the handler

--- examples/basic.js
+++ examples/basic.js
@@ -1,20 +1,17 @@
 var config = require('./config');
 
-var DigitalOcean = require('dropletapi').Droplets;
-
-var digitalOcean = new DigitalOcean(config.token);
-
 var dropletsHandler = require('../src/DropletsHandler');
 
-digitalOcean.listDroplets(function (err, result) {
-  onDropletsData(result);
-})
-
-function onDropletsData (data) {
-  if (data) {
-    dropletsHandler.onDataUpdate(data)
-  }
-}
-
+dropletsHandler.init(config.token);
 dropletsHandler.setOkStatuses(config.okStatuses);
 dropletsHandler.setMonitored(config.monitored);
+
+var onDropletData = function (data) {
+  console.log(data)
+}
+
+dropletsHandler.getData(onDropletData);
+
+setInterval(function () {
+  dropletsHandler.getData(onDropletData);
+}, 60000);
