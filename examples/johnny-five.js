4c1f050a8fe58ce98b21651d23f1e107a9a4174f
rssilva <rafaelspdasilva@gmail.com>
2015-06-04 22:17:35 -0300

    adding a piezo to play song when the status is not ok

--- examples/johnny-five.js
+++ examples/johnny-five.js
@@ -4,36 +4,75 @@ var board = new five.Board({ port : 'COM3' });
 
 var dropletsHandler = require('../src/DropletsHandler');
 
+var led, piezo, alarmInterval;
+
 dropletsHandler.init(config.token);
 dropletsHandler.setOkStatuses(config.okStatuses);
 dropletsHandler.setMonitored(config.monitored);
 
-var onDropletData = function (data, led) {
+var onDropletData = function (data) {
   if (data.ok && data.ok.length) {
     led.stop();
     led.on();
+    stopAlarm();
   }
 
   if (data.notOk && data.notOk.length) {
     led.stop();
     led.strobe();
+    startAlarm();
   }
-}
+};
+
+var startRequests = function () {
+  getData();
+
+  setInterval(function () {
+    getData();
+  }, 30000);
+};
+
+var getData = function () {
+  dropletsHandler.getData(function (data) {
+    onDropletData(data);
+  });
+};
+
+var startAlarm = function () {
+  stopAlarm();
+  playAlarm();
+
+  alarmInterval = setInterval(function () {
+    if (!piezo.isPlaying) {
+      playAlarm();
+    }
+  }, 1000);
+};
+
+var playAlarm = function () {
+  piezo.play({
+    song: "C F D F C F D F C F",
+    beats: 1 / 4,
+    tempo: 100
+  });
+};
+
+var stopAlarm = function () {
+  clearInterval(alarmInterval);
+};
 
 board.on('ready', function() {
   console.log('starting johnny...');
 
-  var led = new five.Led(13);
+  led = new five.Led(13);
+  piezo = new five.Piezo(9);
   
-  dropletsHandler.getData(function (data) {
-    onDropletData(data, led);
+  board.repl.inject({
+    piezo: piezo
   });
 
-  setInterval(function () {
-    dropletsHandler.getData(function (data) {
-    	onDropletData(data, led);
-    });
-  }, 10000);
+  startRequests();
 
-  led.strobe();
+  led.stop();
+  led.on();
 });
